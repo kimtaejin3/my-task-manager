ae9040f3c8c5ab53ee7ec4885dd6b49ee1eba317
kimtaejin3 <rlaxowls1316@naver.com>
2025-08-09 21:26:20 +0900

    refactor: inline layout into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,41 @@
+import styled from "@emotion/styled";
+
 import Dashboard from "./components/dashboard/dashboard";
-import Layout from "./components/layout/layout";
 import Sidebar from "./components/sidebar/sidebar";
 import { SidebarProvider } from "./contexts/SidebarContext";
 
 function App() {
   return (
     <>
-      <Layout
-        sidebar={
+      <LayoutContainer>
+        <SidebarContainer>
           <SidebarProvider>
             <Sidebar />
           </SidebarProvider>
-        }
-        dashboard={<Dashboard />}
-      />
+        </SidebarContainer>
+        <DashboardContainer>
+          <Dashboard />
+        </DashboardContainer>
+      </LayoutContainer>
     </>
   );
 }
+
+const LayoutContainer = styled.div`
+  display: flex;
+  padding: 12px;
+  height: 100vh;
+`;
+
+const SidebarContainer = styled.div`
+  width: fit-content;
+`;
+
+const DashboardContainer = styled.div`
+  background-color: ${(props) => props.theme.themeValue.secondary};
+  border-radius: 12px;
+  padding: 16px 12px;
+  flex-grow: 1;
+`;
 
 export default App;
